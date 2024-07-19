c727acd51b05d4d02e1fe3d673a25a1c15e8945c
sidhantnahak2 <snahak@greenenco.co.uk>
2024-07-20 01:36:25 +0530

    update index.js

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ connectToDatabase();
 
 async function run() {
   try {
-    console.log("wait fetching api");
+    // console.log("wait fetching api");
     const res = await fetchApi(userStationList);
     let data = res?.page?.records;
     if (data) {
-      console.log("Data got from api");
-      console.log("Data size is ", data?.length);
+      // console.log("Data got from api");
+      // console.log("Data size is ", data?.length);
       for (let e of data) {
         await Plant.create({
           timeStamp: e?.dataTimestampStr,
@@ -42,47 +42,41 @@ async function run() {
   }
 }
 
-// setInterval(() => {
-//   // run();
-//   console.log("inside timeout")
-// // }, 10000);
-// schedule.scheduleJob("*/10 * * * * *",  ()=>{
-//   console.log("inside shedule job ",new Date())
-// })
+setInterval(() => {
+  run();
+}, 10000);
 
-const rule1 = new schedule.RecurrenceRule();
-const rule2 = new schedule.RecurrenceRule();
-rule1.tz = 'Etc/UTC';
-rule2.tz='Etc/UTC'
-rule1.minute=59
-rule1.hour=19;
-rule2.minute=5;
-rule2.hour=20;
+// const rule1 = new schedule.RecurrenceRule();
+// const rule2 = new schedule.RecurrenceRule();
+// rule1.tz = 'Etc/UTC';
+// rule2.tz='Etc/UTC'
+// rule1.minute=1
+// rule1.hour=20;
+// rule2.minute=5;
+// rule2.hour=20;
 
+// let job1 = null;
 
-let job1 = null;
+// schedule.scheduleJob(rule1, function () {
+//   if (job1) {
+//     console.log("Fetching plant data job has already been started");
+//   } else {
+//     console.log("Fetching plant data job has been started");
+//     job1 = schedule.scheduleJob("*/1 * * * *", function () {
+//       run();
+//     });
+//   }
+// });
 
-schedule.scheduleJob(rule1, function () {
-  if (job1) {
-    console.log("Fetching plant data job has already been started");
-  } else {
-    console.log("Fetching plant data job has been started");
-    job1 = schedule.scheduleJob("*/1 * * * *", function () {
-      console.log("invoking run!")
-      // run();
-    });
-  }
-});
-
-schedule.scheduleJob(rule2, function () {
-  if (job1) {
-    job1.cancel();
-    job1 = null;
-    console.log("Fetching plant data job has been cancelled");
-  } else {
-    console.log("Fetching plant data job has not been started yet");
-  }
-});
+// schedule.scheduleJob(rule2, function () {
+//   if (job1) {
+//     job1.cancel();
+//     job1 = null;
+//     console.log("Fetching plant data job has been cancelled");
+//   } else {
+//     console.log("Fetching plant data job has not been started yet");
+//   }
+// });
 
 app.get("/data", async (req, res) => {
   res.status(200).json({ message: "Getting response", sucess: true });
