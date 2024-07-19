146b16fe24209e7ca0a8e546419b5a64fbb68b5d
sidhantnahak2 <snahak@greenenco.co.uk>
2024-07-19 12:35:48 +0530

    update in index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { userStationList } = require("./utils/API");
 const Plant = require("./models/energyGenerationSchema");
 const app = express();
 require("dotenv").config({ path: ".env" });
-// const schedule = require("node-schedule");
+const schedule = require("node-schedule");
 
 app.use(cors());
 app.use(express.json());
@@ -17,22 +17,22 @@ connectToDatabase();
 
 async function run() {
   try {
-    console.log("wait fetching api");
+    // console.log("wait fetching api");
     const res = await fetchApi(userStationList);
     let data = res?.page?.records;
     if (data) {
-      console.log("Data got from api");
-      console.log("Data size is ", data?.length);
+      // console.log("Data got from api");
+      // console.log("Data size is ", data?.length);
       for (let e of data) {
         await Plant.create({
           timeStamp: e?.dataTimestampStr,
           energyGeneration: e?.dayEnergy,
           powerGeneration: e?.power,
         });
-        console.log("Document created ");
+        // console.log("Document created ");
       }
     } else {
-      console.log("Error: data not found from api");
+      // console.log("Error: data not found from api");
     }
   } catch (error) {
     console.log(
@@ -42,32 +42,32 @@ async function run() {
   }
 }
 
-setInterval(() => {
-  run();
-}, 10000);
+// setInterval(() => {
+//   run();
+// }, 10000);
 
-// let job1 = null;
+let job1 = null;
 
-// const job2 = schedule.scheduleJob("0 10 11 * * *", function () {
-//   if (job1) {
-//     console.log("Fetching plant data job has already been started");
-//   } else {
-//     console.log("Fetching plant data job has been started");
-//     job1 = schedule.scheduleJob("*/5 * * * *", function () {
-//       run();
-//     });
-//   }
-// });
+const job2 = schedule.scheduleJob("0 0 13 * * *", function () {
+  if (job1) {
+    console.log("Fetching plant data job has already been started");
+  } else {
+    console.log("Fetching plant data job has been started");
+    job1 = schedule.scheduleJob("*/5 * * * *", function () {
+      run();
+    });
+  }
+});
 
-// const job3 = schedule.scheduleJob("0 30 11 * * *", function () {
-//   if (job1) {
-//     job1.cancel();
-//     job1 = null;
-//     console.log("Fetching plant data job has been cancelled");
-//   } else {
-//     console.log("Fetching plant data job has not been started yet");
-//   }
-// });
+const job3 = schedule.scheduleJob("0 5 18 * * *", function () {
+  if (job1) {
+    job1.cancel();
+    job1 = null;
+    console.log("Fetching plant data job has been cancelled");
+  } else {
+    console.log("Fetching plant data job has not been started yet");
+  }
+});
 
 app.get("/data", async (req, res) => {
   res.status(200).json({ message: "Getting response", sucess: true });
