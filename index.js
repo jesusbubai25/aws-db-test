860b26c51e87f9ba90ab4b6194fbe1bb6fea7005
jesusbubai25 <154751893+jesusbubai25@users.noreply.github.com>
2024-07-29 12:44:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 const body_parser = require("body-parser");
@@ -46,6 +47,11 @@ async function run() {
 //   run();
 // }, 10000);
 
+    mongoose.connect(
+      "mongodb://sidh1234:sidh@docdb-cluster-01.cluster-crgmmsg0es4i.ap-south-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+    ).then(res=>console.log("Connected to database successfully! \n Host is ",res.connection.host))
+    .catch(err=>console.log("Error:",err?.message))
+
 const rule1 = new schedule.RecurrenceRule();
 const rule2 = new schedule.RecurrenceRule();
 rule1.tz = 'Etc/UTC';
@@ -58,26 +64,26 @@ rule2.minute=5;
 
 let job1 = null;
 
-schedule.scheduleJob(rule1, function () {
-  if (job1) {
-    console.log("Fetching plant data job has already been started");
-  } else {
-    console.log("Fetching plant data job has been started");
-    job1 = schedule.scheduleJob("*/5 * * * *", function () {
-      run();
-    });
-  }
-});
+// schedule.scheduleJob(rule1, function () {
+//   if (job1) {
+//     console.log("Fetching plant data job has already been started");
+//   } else {
+//     console.log("Fetching plant data job has been started");
+//     job1 = schedule.scheduleJob("*/5 * * * *", function () {
+//       run();
+//     });
+//   }
+// });
 
-schedule.scheduleJob(rule2, function () {
-  if (job1) {
-    job1.cancel();
-    job1 = null;
-    console.log("Fetching plant data job has been cancelled");
-  } else {
-    console.log("Fetching plant data job has not been started yet");
-  }
-});
+// schedule.scheduleJob(rule2, function () {
+//   if (job1) {
+//     job1.cancel();
+//     job1 = null;
+//     console.log("Fetching plant data job has been cancelled");
+//   } else {
+//     console.log("Fetching plant data job has not been started yet");
+//   }
+// });
 
 app.get("/data", async (req, res) => {
   res.status(200).json({ message: "Getting response", sucess: true });
